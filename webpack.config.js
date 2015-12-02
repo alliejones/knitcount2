84b8bb08e0b367254728d0149cc45b2fae0f8f46
Allie Jones <allie@alliejon.es>
2015-12-02 18:26:00 -0500

    Fix hot loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 var webpack = require('webpack');
 module.exports = {
-  entry: "./js/app.jsx",
+  entry: [
+    "./js/app.jsx"
+  ],
   output: {
     path: __dirname + '/build',
-    filename: "bundle.js"
+    filename: "bundle.js",
+    publicPath: '/'
   },
   module: {
     loaders: [
       { test: /\.jsx?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
-      { test: /\.css$/, loader: "style!css" }
+      { test: /\.s?css$/, loaders: ['style', 'css', 'sass'] }
     ]
   },
   resolve: {
